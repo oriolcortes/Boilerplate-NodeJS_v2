7b4b7d57722c98895acdb68c1584b31b6a0064c2
oriolcortes <oriol.cortesc@gmail.com>
2025-04-22 13:11:44 +0200

    update baseURL log

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,11 @@ import { app } from './app.js';
 import { PORT } from './config/config.js';
 import logger from './config/logger.js';
 
+const BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? `https://${process.env.RENDER_EXTERNAL_HOSTNAME}`
+    : `http://localhost:${PORT}`;
+
 app.listen(PORT, () => {
-  logger.info(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+  logger.info(`⚡️[server]: Server is running at ${BASE_URL}`);
 });
